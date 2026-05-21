8d5f8ecbb612bfcbd816f82ee61f9ef79565fec8
setys <wi25b018@technikum-wien.at>
2026-05-21 15:23:09 +0200

    Implement US-01 choose empty square

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,47 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final char EMPTY = '-';
+
     public static void main(String[] args) {
-        System.out.println("TicTacToe started");
+        char[] board = {
+                EMPTY, EMPTY, EMPTY,
+                EMPTY, EMPTY, EMPTY,
+                EMPTY, EMPTY, EMPTY
+        };
+
+        Scanner scanner = new Scanner(System.in);
+
+        printBoard(board);
+
+        System.out.print("Choose an empty square from 1 to 9: ");
+        int square = scanner.nextInt();
+
+        if (square < 1 || square > 9) {
+            System.out.println("Invalid square. Please choose a number from 1 to 9.");
+            return;
+        }
+
+        int index = square - 1;
+
+        if (board[index] != EMPTY) {
+            System.out.println("This square is already taken.");
+            return;
+        }
+
+        board[index] = 'X';
+
+        System.out.println("Move accepted.");
+        printBoard(board);
+    }
+
+    private static void printBoard(char[] board) {
+        System.out.println();
+        System.out.println(board[0] + " | " + board[1] + " | " + board[2]);
+        System.out.println(board[3] + " | " + board[4] + " | " + board[5]);
+        System.out.println(board[6] + " | " + board[7] + " | " + board[8]);
+        System.out.println();
     }
 }
